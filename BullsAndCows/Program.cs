ad7acd003b01ffdeeda7277ccbd9947d49476e4b
Raskovalov <149192370+Raskovalov@users.noreply.github.com>
2023-11-01 14:28:09 +0300

    Add files via upload

---
+++ BullsAndCows/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using BullsAndCows;
+
+namespace BullsAndCows
+{
+    class BullCows
+    {
+        Random rand = new Random();
+
+        public int number;
+        public int bulls;
+        public int cows;
+
+        public BullCows()
+        {
+            this.number = rand.Next(0, 100);
+            this.bulls = 0;
+            this.cows = this.number.ToString().Length;
+        }
+
+        List<int> BullsCows(int num)
+        {
+            List<int> BullsCows = new List<int>();
+            int x = 0;
+
+            foreach(char i in Convert.ToString(num)) 
+            {
+                foreach(char u in Convert.ToString(this.number)) 
+                {
+                    if (u == i)
+                    {
+                        x += 1;
+                        break;
+                    }
+                }
+            }
+
+            BullsCows.Add(x);
+            BullsCows.Add(this.cows - x);
+
+            return BullsCows;
+        }
+
+        int ReadNumber()
+        {
+            Console.Write("Введите число: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            return num;
+        }
+
+        void go()
+        {
+            int x = 0;
+
+            Console.WriteLine($"Коров: {this.cows}");
+            while (true)
+            {
+                int ev = 1;
+                int num = ReadNumber();
+                List<int> bullsCows = new List<int>();
+                bullsCows = BullsCows(num);
+
+                if (bullsCows[1] == 0) { 
+                    if (num == this.number)
+                    {
+                        Console.WriteLine($"Игра закончена, Вы выйграли, отгаданное число: {this.number},   Попыток: {x}");
+                        ev = 0;
+                        break;
+                    }
+                }
+
+                if(ev == 1)
+                {
+                    Console.WriteLine($"Коров: {bullsCows[1]}, Быков: {bullsCows[0]}, Попытка: {x}, Число: {this.number}");
+                }
+
+                x += 1;
+            }
+        }
+
+        static void Main()
+        {
+            BullCows b = new BullCows();
+            b.go();
+        }
+    }
+}
+
